0ab92e64a0accc0f657e806e6b239b0122b97a5a
Ricardo Bustamante <ricardo@busta.dev>
2026-01-31 10:27:12 -0300

    Load and display characters

---
+++ furto/code/vn/character_root.gd
@@ -0,0 +1,5 @@
+extends Control
+
+class_name CharacterRoot
+
+@export var char_name: String
--- furto/code/vn/gameplay_controller.gd
+++ furto/code/vn/gameplay_controller.gd
@@ -4,8 +4,10 @@ extends Node
 @onready var vn_text_label : RichTextLabel = $VNCanvas/Root/TextPanel/TextLabel
 @onready var vn_fade_panel : ColorRect = $VNCanvas/Root/FadePanel
 @onready var location_canvas : CanvasLayer = $LocationCanvas
+@onready var character_canvas: CanvasLayer = $CharacterCanvas
 
-@export var location_packed : PackedScene = preload("res://assets/locations/placeholder/placeholder_location.tscn")
+@export var placeholder_location: String = "res://assets/locations/placeholder/placeholder_location.tscn"
+@export var placeholder_character: String = "res://assets/characters/placeholder/placeholder_character.tscn"
 
 var location_node: LocationRoot
 const vn_text_panel_on_y: int = 780
@@ -14,15 +16,30 @@ const vn_text_animate_duration: float = 0.4
 const vn_text_animate_text_duration: float = 1.5
 
 func _ready() -> void:
-	await async_set_location("res://assets/locations/placeholder/placeholder_location.tscn", 0.0)
+	var char1:= await async_load_character(placeholder_character)
+	var char2:= await async_load_character(placeholder_character)
+	
+	await async_set_location(placeholder_location, 0.0)
 	vn_text_panel.visible = false
 	vn_fade_panel.visible = false
 	await async_show_text("Lorem ipsum")
 	await async_show_text("Testing something")
-	await async_set_location("res://assets/locations/placeholder/placeholder_location.tscn", 1.0)
+	await async_set_location(placeholder_character, 1.0)
 	await async_show_text("a")
 	await async_show_text("b")
 	
+func async_load_character(res: String) -> CharacterRoot:
+	var packed_character := load(res)
+	var char: CharacterRoot = packed_character.instantiate()
+	char.name = char.char_name
+	character_canvas.add_child(char)
+	char.visible = false
+	return char
+
+func unload_all_characters():
+	for child in character_canvas.get_children():
+		child.queue_free()
+
 func async_set_location(res: String, fade_duration: float) -> void:
 	if fade_duration > 0 and location_node:
 		vn_fade_panel.visible = true
