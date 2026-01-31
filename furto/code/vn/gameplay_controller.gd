3762557ee9e91a47a381e91b1e7d42c59d406100
Ismália Santiago <ismalia.santiago@g.globo>
2026-01-31 18:45:54 -0300

    Add assets and scripts for investigation parts

--- furto/code/vn/gameplay_controller.gd
+++ furto/code/vn/gameplay_controller.gd
@@ -1,7 +1,15 @@
 extends Node
 
+class_name GameplayController
+
 @onready var vn_controller: VNController = $VNController
 @export var placeholder_location: String = "res://assets/locations/placeholder/placeholder_location.tscn"
+@export var placeholder_background_investigation_location: String = "res://assets/locations/placeholder_background_investigation/placeholder_background_investigation_location.tscn"
+var clicked: ClickableRoot = null
+
+func _init() -> void:
+	Engine.register_singleton(&"GameplayController", self)
+
 
 func _ready() -> void:
 	var fursuit_maker:= vn_controller.load_character("res://assets/characters/fursuit_maker/fursuit_maker.tscn")
@@ -24,6 +32,6 @@ func _ready() -> void:
 			break
 	
 	await vn_controller.show_text("E todos viveram felizes para sempre", null)
-	await vn_controller.set_location(placeholder_location, 1.0)
-	await vn_controller.show_text("a", null)
-	await vn_controller.show_text("b", null)
+	await vn_controller.set_location(placeholder_background_investigation_location, 1.0)
+	#await vn_controller.show_text("a", null)
+	#await vn_controller.show_text("b", null)
